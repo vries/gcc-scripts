1d3c411271f21da879c7ddf2429b5cd7eebfc1e2
Tom de Vries <tom@codesourcery.com>
2015-05-20 09:12:17 +0200

    install_requirements.sh: add installation of gcc-x.y-multilib

--- install_requirements.sh
+++ install_requirements.sh
@@ -7,3 +7,16 @@ packages="g++ m4 flex libc6-dev-i386 texinfo autogen dejagnu gnat gawk"
 for p in ${packages}; do
   sudo apt-get -y install ${p};
 done
+
+if gnat > /dev/null; then
+    # To get gnat support
+    v=$(gnat \
+	| grep GNAT \
+	| awk '{print $2}')
+    vbase=$(echo "$v" \
+	| sed 's%\([0-9]\.[0-9]*\).*%\1%')
+
+    sudo apt-get -y install gcc-$vbase-multilib
+fi
+
+
