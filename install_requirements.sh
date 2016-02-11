ce7356e8228069a434b567047025104edf6004e1
Tom de Vries <tom@codesourcery.com>
2016-02-11 22:55:06 +0100

    install_requirements.sh: add libelf-dev

--- install_requirements.sh
+++ install_requirements.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-packages="g++ m4 flex libc6-dev-i386 texinfo autogen dejagnu gnat gawk"
+packages="g++ m4 flex libc6-dev-i386 texinfo autogen dejagnu gnat gawk libelf-dev"
 
 for p in ${packages}; do
   sudo apt-get -y install ${p};
